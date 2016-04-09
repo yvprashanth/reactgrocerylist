472b1ca6847022e0250320fd50f4c48b8359b345
PrashMaya2 <z@Mac-User-3.local>
2016-04-09 11:01:45 -0700

    initial set

---
+++ server/main.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.render('./../app/index.ejs', {});
+})
+.use(express.static(__dirname + '/../app'))
+.listen(7777);
